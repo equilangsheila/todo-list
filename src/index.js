79bec6b88859aff870b9c8922efdf50558250284
Sheila Equilang <equilangsheila@gmail.com>
2022-01-20 16:31:43 +0800

    Change inline Style

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+
 // My first component
 function App() {
+    const paraStyle = {
+        "marginBottom": "15px"
+    }
+
   return (
       <div className="app">
           <header className="app-header">
               <h1>
                   My Amazing ToDo-List App
               </h1>
-              <p>The most simple and amazing todo-list React app.</p>
+              <p style={paraStyle}>The most simple and amazing todo-list React app.</p>
+              <ul>
+                  <li className="tasks-item">Follow me on Facebook</li>
+                  <li className="tasks-item">Follow me on Instagram</li>
+                  <li className="tasks-item">Follow me on Twitter</li>
+              </ul>
           </header>
       </div>
   )
