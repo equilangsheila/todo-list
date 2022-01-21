745f4a0299fd576e7b1db47f7927c2f56075dea9
Sheila Equilang <equilangsheila@gmail.com>
2022-01-21 15:00:18 +0800

    Input taskHandler and useState

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,4 +1,5 @@
 import Task from "./Task";
+import {useState} from "react";
 
 function TaskList (){
     const taskItemsList = [
@@ -6,15 +7,29 @@ function TaskList (){
         "Follow me on Instagram",
         "Follow me on Twitter"
     ];
+
+    const [taskValue, setTaskValue] = useState("Just another task");
+    console.log(taskValue);
+
+    const inputChangeHandler = (e) => {
+        setTaskValue(e.target.value)
+    };
+    
     return (
         <div>
-            <input className="task-input"/>
+          <input
+        className="task-input"
+        placeholder="Create a new task"
+        onChange={inputChangeHandler}
+      />
+
+             {/* <input className="task-input" placeholder="Create a new task"/> */}
                 <ul>
                   {taskItemsList.map ((task, index) => {
                   return <Task key={index} taskName={task}/>
             })}
               </ul>
-        </div>
+              </div>
     );
 }
 
