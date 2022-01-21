b5a1142f4649e769c976e8b2531acfcf491474ab
Sheila Equilang <equilangsheila@gmail.com>
2022-01-21 15:23:43 +0800

    Update Data to an array list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,25 +2,31 @@ import Task from "./Task";
 import {useState} from "react";
 
 function TaskList (){
-    const taskItemsList = [
+    const [ taskItemsList, setTaskItemsList ] = useState ([
         "Follow me on Facebook",
         "Follow me on Instagram",
         "Follow me on Twitter"
-    ];
+    ]);
 
-    const [taskValue, setTaskValue] = useState("Just another task");
+    const [taskValue, setTaskValue] = useState("");
     console.log(taskValue);
 
     const inputChangeHandler = (e) => {
         setTaskValue(e.target.value)
     };
+
+    const addTaskHandler = (e) => {
+        setTaskItemsList([taskValue, ...taskItemsList]);
+      };
     
     return (
         <div>
-          <input
+    <input
         className="task-input"
         placeholder="Create a new task"
         onChange={inputChangeHandler}
+        onBlur={addTaskHandler}
+        value={taskValue}
       />
 
              {/* <input className="task-input" placeholder="Create a new task"/> */}
